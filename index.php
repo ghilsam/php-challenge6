b63cb2e3f8dbc70b762a3ecd9bbd8272b1f4100d
aghilas <aghil.naitamer@gmail.com>
2021-11-02 17:58:49 +0100

    fonction

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function writeSecretSentence($param1, $param2){
+return $param1. ' s\'incline face à ' .$param2;
+
+}
+echo writeSecretSentence('la chauve souris', 'la lumiére' );
+
+?>
+
+
